507d9366877b51d33636efee2c20fb08128896e0
Wiedzrin <wiedzrin@gmail.com>
2021-04-18 04:09:17 +0300

    Разработка меню

--- src/main/js/view/src/app/app.module.ts
+++ src/main/js/view/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule} from "@angular/material/button";
+
+import {MenuComponent} from "./header/menu/menu.component";
+import { SearchComponent } from './header/search/search.component';
+import { UserMenuComponent } from './header/user-menu/user-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SearchComponent,
+    UserMenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
